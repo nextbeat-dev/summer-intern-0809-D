10eb8adf1dd6889aaae9a787c57f9af2972786de
Miyajima Kenta <shadowlink0122@gmail.com>
2019-08-10 11:26:30 +0900

    create idol list controller

---
+++ app/controllers/idol/IdolController.scala
@@ -0,0 +1,30 @@
+package controllers.idol
+
+import play.api.i18n.I18nSupport
+import play.api.mvc.{AbstractController, MessagesControllerComponents}
+import persistence.idol.dao.IdolDao
+import persistence.idol.model.Idol
+import model.site.product.SiteViewIdolList
+import model.component.util.ViewValuePageLayout
+
+class IdolController @javax.inject.Inject()(
+  val idolDao: IdolDao,
+  cc: MessagesControllerComponents
+) extends  AbstractController(cc) with I18nSupport {
+  implicit lazy val executionContext = defaultExecutionContext
+
+  /**
+    * アイドル一覧ページ
+    */
+  def list = Action.async { implicit request =>
+    for{
+      idolSeq <- idolDao.findAll
+    } yield {
+      val vv = SiteViewIdolList(
+        layout = ViewValuePageLayout(id = request.uri),
+        idol   = idolSeq
+      )
+      Ok(views.html.site.idol.list.Main(vv))
+    }
+  }
+}
