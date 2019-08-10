025b342b96f68713207ee344b7f36f4f2f14d872
Miyajima Kenta <shadowlink0122@gmail.com>
2019-08-10 19:36:35 +0900

    Details of product in backend process is completed.

--- app/controllers/product/ProductController.scala
+++ app/controllers/product/ProductController.scala
@@ -2,13 +2,18 @@ package controllers.product
 
 import play.api.i18n.I18nSupport
 import play.api.mvc.{AbstractController, MessagesControllerComponents}
+import persistence.idol.dao.IdolDao
+import persistence.idol_products.dao.IdolProductsDAO
 import persistence.product.dao.ProductDAO
 import persistence.product.model.Product
-import model.site.product.SiteViewValueProduct
+import model.site.idol.SiteViewIdolDetail
+import model.site.product.SiteViewProductDetail
 import model.component.util.ViewValuePageLayout
 
 // 商品
 class ProductController @javax.inject.Inject()(
+  val idolDao   : IdolDao,
+  val idolProductDao: IdolProductsDAO,
   val productDao: ProductDAO,
   cc: MessagesControllerComponents
 ) extends  AbstractController(cc) with I18nSupport {
@@ -20,14 +25,25 @@ class ProductController @javax.inject.Inject()(
 
   def detail(idolId: Long, productId: Long) = Action.async { implicit request =>
     for {
-      product <- productDao.get(productId)
+      idol     <- idolDao.get(idolId)
+      prdoucts <- idolProductDao.getProductsByIdolid(idolId)
+
+      product  <- productDao.get(productId)
     } yield {
-      val vv = SiteViewValueProduct(
-        layout     = ViewValuePageLayout(id = request.uri),
+      val vvIdol = SiteViewIdolDetail(
+        layout  = ViewValuePageLayout(id = request.uri),
+        idol = idol.get,
+        products = prdoucts
+      )
+
+      val vvProduct = SiteViewProductDetail(
+        layout  = ViewValuePageLayout(id = request.uri),
         product = product
       )
+      Ok(views.html.site.product.detail.Main(vvIdol, vvProduct))
+
       // とりあえずrecruitページに飛ぶ
-      Redirect("/recruit/intership-for-summer-21")
+      // Redirect("/recruit/intership-for-summer-21")
     }
   }
 }
